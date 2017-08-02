65a5b9442971694e2e768646a03dc3a628d23d5f
Azbus <onix.09@mail.ru>
2017-08-02 12:35:34 +0300

    Added Queue

--- DataStructures/MyQueue.cs
+++ DataStructures/MyQueue.cs
@@ -6,7 +6,72 @@ using System.Threading.Tasks;
 
 namespace DataStructures
 {
-    class MyQueue
+    public class MyQueue<T>
     {
+        private T[] array;
+        private int length;
+        private int head;
+        private int tail;
+        private int defaultLength = 10;
+
+        public MyQueue()
+        {
+            head = 0;
+            tail = 0;
+            length = defaultLength;
+            array = new T[length];
+        }
+        public MyQueue(int length)
+        {
+            head = 0;
+            tail = 0;
+            this.length = length;
+            array = new T[this.length];
+        }
+
+        public void Enqueue(T x)
+        {
+            if (isFull())
+                throw new Exception("Overflow");
+
+            array[tail] = x;
+            if (tail == length)
+                tail = 1;
+            else
+                tail++;
+        }
+        public T Dequeue()
+        {
+            if (isEmpty())
+                throw new Exception("UnderFlow");
+
+            T x = array[head];
+            if (head == length)
+                head = 1;
+            else
+                head++;
+            return x;
+        }
+
+        public int getLength()
+        {
+            return length;
+        }
+
+        public bool isEmpty()
+        {
+            if (tail == head)
+                return true;
+            else
+                return false;
+        }
+
+        public bool isFull()
+        {
+            if ((head == tail + 1) || (head == 0 && tail == length))
+                return true;
+            else
+                return false;
+        }
     }
 }
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -10,7 +10,8 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            StackTest();
+            //StackTest();
+            QueueTest();
         }
 
         static void StackTest()
@@ -19,7 +20,6 @@ namespace DataStructures
             MyStack<int> stack = new MyStack<int>();
             Console.WriteLine("\n Stack is empty: " + stack.isEmpty());
 
-            Console.WriteLine("Try to PUSH nums: 0-19 ");
             for (int i = 0; i < N; i++)
             {
                 Console.WriteLine("Try to PUSH num: " + i);
@@ -34,6 +34,33 @@ namespace DataStructures
             }
 
             Console.WriteLine("\n Stack is empty: " + stack.isEmpty());
+
+            Console.Read();
+        }
+        static void QueueTest()
+        {
+            int N = 8;
+            MyQueue<int> queue = new MyQueue<int>();
+            Console.WriteLine("\n Queue is full: " + queue.isFull());
+            Console.WriteLine(" Queue is empty: " + queue.isEmpty());
+            
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Try to Enqueue num: " + i);
+                queue.Enqueue(i);
+            }
+
+            Console.WriteLine("\n Queue is full: " + queue.isFull());
+            Console.WriteLine(" Queue is empty: " + queue.isEmpty());
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Try to Dequeue, number is: " + queue.Dequeue());
+            }
+
+            Console.WriteLine("\n Queue is full: " + queue.isFull());
+            Console.WriteLine(" Queue is empty: " + queue.isEmpty());
 
             Console.Read();
         }
