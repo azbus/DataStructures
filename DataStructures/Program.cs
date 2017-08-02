17da5a9fa9725763650ec8b7172dfd53128d2550
Azbus <onix.09@mail.ru>
2017-08-02 11:37:21 +0300

    Добавьте файлы проекта.

---
+++ DataStructures/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackTest();
+        }
+
+        static void StackTest()
+        {
+            int N = 20;
+            MyStack<int> stack = new MyStack<int>();
+            Console.WriteLine("\n Stack is empty: " + stack.isEmpty());
+
+            Console.WriteLine("Try to PUSH nums: 0-19 ");
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Try to PUSH num: " + i);
+                stack.Push(i);
+            }
+
+            Console.WriteLine("\n Stack is empty: " + stack.isEmpty());
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Try to PULL, number is: " + stack.Pop());
+            }
+
+            Console.WriteLine("\n Stack is empty: " + stack.isEmpty());
+
+            Console.Read();
+        }
+    }
+}
---
+++ DataStructures/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class MyStack<T>
+    {
+        private int top;
+        private T[] array;
+
+        public MyStack()
+        {
+            top = 0;
+            array = new T[top];
+        }
+
+        public bool isEmpty()
+        {
+            if (this.top == 0)
+                return true;
+            else
+                return false;
+        }
+        public void Push(T x)
+        {
+            top++;
+            Array.Resize(ref array, top);
+            array[top-1] = x;
+        }
+        public T Pop()
+        {
+            if (isEmpty())
+                throw new Exception("Underflow");
+            else
+                top--;
+            T x = array[top];
+            Array.Resize(ref array, top);
+            return x;
+        }
+    }
+}
